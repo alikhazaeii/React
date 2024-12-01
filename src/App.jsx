bc89007026dec65106017eb56c473a5878ba58be
Ali <165763641+alikhazaeii@users.noreply.github.com>
2024-12-01 20:06:45 +0330

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
-import React from 'react'
-import Header from './assets/Components/header/header'
-import { Body } from './assets/Components/body/body'
-import Footer from './assets/Components/footer/footer'
 
+import React, { useEffect, useState } from 'react'
+import bg from './../public/bg.jpg'
 export default function App() {
-  return (
-    <main>
-         <Header/>
-         <Body/>
-         <Footer/>
-    </main>
-  )
+  const [data, setData] = useState([])
+  useEffect(() => {
+    const fetchData = async () => {
+      const url = 'https://fakestoreapi.com/products/';
+
+      try {
+        const response = await fetch(url);
+        const result = await response.json();
+
+        // console.log(result);
+        setData(result)
+
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData()
+    return (
+      <div className='w-screen h-screen bg-[#141920] flex flex-wrap'>
+        <img className='w-full h-full fixed top-0 left-0' src={bg} alt="" />
+        {result && result.map(val => {
+          console.log(result);
+          
+          <article className='w-8/12 h-[200px] bg-red-500 relative z-10' ></article>
+        })}
+
+      </div>
+    )
+  }, [])
+
+
+
+
 }
+
+
